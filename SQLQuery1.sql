967ea6d11696d04026fba62c034df304965127ef
oboba228 <92936368+oboba228@users.noreply.github.com>
2023-05-16 18:42:26 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE table_aboba228 (
+    name varchar(255),
+	ingredients char,
+	mass int,
+	additives char,
+	cabbage char,
+	cucumber char,
+	tomato char,
+	sauce char,
+	tortilla char
+);
